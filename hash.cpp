f812812fdc7032f632e4961e9ef991f16b94db2d
alf3ratz <apetropavlovskij@yandex.ru>
2024-03-15 01:21:25 +0300

    [hse-sys-prog2] Init

--- hash.cpp
+++ hash.cpp
@@ -1,4 +1,5 @@
 #include <openssl/evp.h>
+#include <openssl/md5.h>
 
 #include <iomanip>
 #include <iostream>
@@ -35,7 +36,7 @@ std::vector<std::string> generateCombinations(int length) {
 
 std::string calculateMD5(const std::string &str) {
     EVP_MD_CTX *mdContext = EVP_MD_CTX_new();
-    const EVP_MD *md = EVP_md5();  // Use md5() for MD5
+    const EVP_MD *md = EVP_md5();
 
     EVP_DigestInit_ex(mdContext, md, nullptr);
     EVP_DigestUpdate(mdContext, str.c_str(), str.length());
@@ -53,47 +54,55 @@ std::string calculateMD5(const std::string &str) {
 }
 
 void parallelHashCrack(const std::string &hashToCrack, int start, int end) {
+    std::thread::id threadId = std::this_thread::get_id();
+    {
+        std::lock_guard<std::mutex> guard(printMutex);
+        std::cout << "Thread id: " << threadId << '\n';
+    }
+
     for (int i = start; i <= end; i++) {
         // std::vector<std::string> result = generateCombinations(i);
         // for (const auto &combination : result) {
         std::string result = calculateMD5(std::to_string(i));
+
         if (result == hashToCrack) {
             std::lock_guard<std::mutex> guard(printMutex);
-            std::cout << "Найдено совпадение: " << i << '\n';
+            std::cout << "Found a match: " << i << "at thread id: " << threadId
+                      << '\n';
             return;
         }
         // }
     }
-    std::cout << "Совпадение не найдено :(" << '\n';
+    std::cout << "No match found :(" << '\n';
 }
 
-// int main(int argc, char *argv[]) {
-//     if (argc != 2) {
-//         std::cerr << "You should enter hash as argument!" << '\n';
-//         return 1;
-//     }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "You should enter hash as argument!" << '\n';
+        return 1;
+    }
 
-//     std::string hashToCrack = argv[1];
+    std::string hashToCrack = argv[1];
 
-//     // Автоматически выбираем количество процессов
-//     int numThreads = std::thread::hardware_concurrency();
+    // Задаем кол-во процессов
+    int numThreads = std::thread::hardware_concurrency();
 
-//     // Делим задачу на интервалы для каждого потока
-//     int valuesPerThread = 1000000000;
-//     std::vector<std::thread> threads;
+    // Размер интервала для каждого потока
+    int valuesPerThread = 50000;
+    std::vector<std::thread> threads;
 
-//     for (int i = 0; i < numThreads; i++) {
-//         int start = i * valuesPerThread;
-//         int end = (i + 1) * valuesPerThread - 1;
+    for (int i = 0; i < numThreads; i++) {
+        int start = i * valuesPerThread;
+        int end = (i + 1) * valuesPerThread - 1;
 
-//         threads.emplace_back(parallelHashCrack, std::ref(hashToCrack), start,
-//                              end);
-//     }
+        threads.emplace_back(parallelHashCrack, std::ref(hashToCrack), start,
+                             end);
+    }
 
-//     // Дожидаемся завершения всех потоков
-//     for (auto &thread : threads) {
-//         thread.join();
-//     }
+    // Завершаем все потоки
+    for (auto &thread : threads) {
+        thread.join();
+    }
 
-//     return 0;
-// }
+    return 0;
+}
